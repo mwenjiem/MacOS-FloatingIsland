a9dff9a52d055305addcd924cc84ceb4e8d398f6
Wenjie Ma <mwenjiem@gmail.com>
2024-10-31 09:29:13 -0700

    Initial Commit

---
+++ FloatingIsland/FloatingIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingIslandApp.swift
+//  FloatingIsland
+//
+//  Created by Wenjie Ma on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloatingIslandTests/FloatingIslandTests.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingIslandTests.swift
+//  FloatingIslandTests
+//
+//  Created by Wenjie Ma on 10/31/24.
+//
+
+import Testing
+@testable import FloatingIsland
+
+struct FloatingIslandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
